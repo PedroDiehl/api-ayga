6da531070c1e51fa538a1424844f8c2704151e49
Pedro Diehl <diehl.pedroh@gmail.com>
2022-05-17 01:40:28 -0300

    Vai rolar?

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
 '''
 Função principal para armazenar a REST API desenvolvida como parte do
 processo seletivo para developer backend da Ayga IOT.
-
 Candidato: Pedro Henrique Diehl
 '''
 
 
 from flask import Flask, request
-#from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, abort, Resource, reqparse
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/', methods=['GET'])
-def HelloWorld():
+class HelloWorld(Resource):
     '''
     teste teste teste
     '''
 
-    return "beyblade"
+    def get(self, name):
+        return {"data": name}
 
+api.add_resource(HelloWorld, '/<string:name>')
 
+if __name__ == "__main__":
+    app.run(debug=False)
