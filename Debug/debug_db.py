82cf16c18b036c9da45a224dd7c1171635603030
Pedro Diehl <diehl.pedroh@gmail.com>
2022-05-17 21:17:29 -0300

    adicionando debug db inicial

--- Debug/debug_db.py
+++ Debug/debug_db.py
@@ -3,6 +3,28 @@ Módulo utilizado para debugar o banco de dados e suas querys
 '''
 
 import json
+import sqlite3
+
+def create_db(db_file):
+    '''
+    Função para criar o banco de dados
+    '''
+
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
+
+    # Create a table named signals with 4 columns
+    # Column 1: ID
+    # Column 2: date in the format YYYY-MM-DDTHH:MM:SS.SSSZ
+    # Column 3: type
+    # Column 4: value
+
+    c.execute("CREATE TABLE signals (id INTEGER PRIMARY KEY, date TEXT, type TEXT, value TEXT)")
+
+    conn.commit()
+    conn.close()
+
+    return
 
 def debug_db(json_file):
     '''
@@ -30,5 +52,31 @@ def debug_db(json_file):
 
     return
 
+def menu_debugdb():
+    '''
+    Menu de terminal para escolher qual operação realizar
+    '''
+
+    print("\n-----------------------------\nEscolha o que deseja testar:\n-----------------------------\n")
+    print("1 - Criar Banco de Dados")
+    print("2 - Acessar debug")
+    print("Outro - ENCERRAR\n")
+
+    while True:
+        escolha = input("Escolha: ")
+
+        if escolha == "1":
+            db_file = "B66db.sqlite"
+            create_db(db_file)
+
+        elif escolha == "2":
+            debug_db("example_json.json")
+
+        else:
+            print("Encerrando...\n")
+            break
+
+    return
+
 if __name__ == "__main__":
-    debug_db("example_json.json")
+    menu_debugdb()
